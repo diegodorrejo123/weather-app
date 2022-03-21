3c574722013c1ed1748fad30acc4f28b8c6b84ec
Diego Dorrejo <dorrejo14@gmail.com>
2022-03-21 00:24:22 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const forecast = require('./utils/forecast');
+
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+app.use(express.static(publicDirectoryPath))
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Diego'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page',
+        name: 'Diego'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Diego'
+    })
+})
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Diego',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    let address = req.query.address;
+    if(!address){
+        return res.send({
+            error: "You must provide an address."
+        })
+    }
+    forecast(address, (error, {text, location} = {}) => {
+        if(error){
+            return res.send({error})
+        }
+        res.send({
+            forecast: text,
+            location,
+            address
+        })
+    })
+})
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found.',
+        name: 'Diego'
+    })
+})
+
+
+
+app.listen(3000, () => { 
+    console.log('Server is up on port 3000.');
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (address, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=04b0302bc836500ad237442b38e2202b&query=' + address
+
+    request({ url, json: true }, (error, { body } = {}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, {text: body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.', location: body.request.query})
+        }
+    })
+}
+
+module.exports = forecast
