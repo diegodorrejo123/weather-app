b08d4096786dd6b2dd065d323ad6ef73aa277c2e
Diego Dorrejo <dorrejo14@gmail.com>
2022-03-21 22:51:55 -0400

    add humidity to response

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (address, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=04b0302bc836500ad237442b38e2202b&query=' + address
 
@@ -9,7 +11,13 @@ const forecast = (address, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, {text: body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.', location: body.request.query})
+            callback(undefined, {
+                text: 
+                    body.current.weather_descriptions + '. It is currently ' + body.current.temperature + 
+                    ' degress out. There is a ' + body.current.precip + '% chance of rain.' +
+                    'The humidity is ' + body.current.humidity + '%.', 
+                location: body.request.query
+            })
         }
     })
 }
